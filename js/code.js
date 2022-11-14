f76421fba0fae10c3e73aa7c0dd15b9d8fbfcab9
Reece1109 <reecesimon84@gmail.com>
2022-11-14 15:59:20 +0200

    Update

---
+++ js/code.js
@@ -0,0 +1,25 @@
+
+// Number 5 of the questions
+function evenData(data) {
+    for(let i=0; i<=data.length; i+=2) {
+        console.log(data[i]);
+    }
+}
+let data =[9, 6, 12, 14, 'Reece'];
+evenData(data)
+
+
+// // shortcut
+// function evenData(data) {
+//     for (let i in data) {
+
+//     }
+// }
+
+do{
+
+}while(i < 10);
+
+while(i < 10) {
+
+}
