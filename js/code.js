6cc0bd856679fb394eb3c231a84e5a07fdde328b
Reece1109 <reecesimon84@gmail.com>
2022-11-14 16:41:20 +0200

    Update2

--- js/code.js
+++ js/code.js
@@ -1,25 +1,44 @@
 
-// Number 5 of the questions
-function evenData(data) {
-    for(let i=0; i<=data.length; i+=2) {
-        console.log(data[i]);
-    }
-}
-let data =[9, 6, 12, 14, 'Reece'];
-evenData(data)
-
-
-// // shortcut
+// // Number 5 of the questions
 // function evenData(data) {
-//     for (let i in data) {
-
+//     for(let i=0; i<=data.length; i+=2) {
+//         console.log(data[i]);
 //     }
 // }
+// let data =[9, 6, 12, 14, 'Reece'];
+// evenData(data)
 
-do{
 
-}while(i < 10);
+// // // shortcut
+// // function evenData(data) {
+// //     for (let i in data) {
 
-while(i < 10) {
+// //     }
+// // }
 
-}
+// do{
+
+// }while(i < 10);
+
+// while(i < 10) {
+
+// }
+
+// const person = {
+//     name: "Reece",
+//     surname: "Simon",
+// }
+//         console.log(person.name);
+//         console.log(person.surname);
+//         person.emailAdress = "Reece@gmail.com";
+//         console.log(person);
+
+
+
+//         function userDetails(firstName, suranme) {
+//             return{
+//                 firstName: firstName,
+//                 surname: surname,
+//                 display: fucntion(){}
+//                     console.log('Name: ${firstName)
+        
